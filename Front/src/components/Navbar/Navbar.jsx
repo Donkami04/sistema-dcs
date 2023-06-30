19738a2301ce4ab407b97e97a0af7e78488da7d9
Camilo Munera <camilo.munerac@gmail.com>
2023-06-30 16:17:48 -0500

    COrreccion UPS finalizado - add Home button

--- Front/src/components/Navbar/Navbar.jsx
+++ Front/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import "./navbar.css";
 
+const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT;
+
+let envi;
+if (ENVIRONMENT === "local") {
+  envi = "localhost";
+} else if (ENVIRONMENT === "development") {
+  envi = "10.224.116.78";
+} else if (ENVIRONMENT === "production") {
+  envi = "10.224.116.14";
+}
+
+
+const BASE_API_URL = `http://${envi}:3000/api/v1/candelaria`;
+console.log(BASE_API_URL)
+
 export function Navbar(props) {
   return (
     <nav className="navbar">
       <div className="logo">
-        <a href="http://localhost:4000/concentradora/dcs/home">
+        <a href={`http://${envi}:4000/concentradora/dcs/home`}>
           <img src="/candelaria.png" alt="Logo" />
         </a>
       </div>
+        <p className="home-link" ><a href={`http://${envi}:4000/concentradora/dcs/home`} style={{color:"white"}}>Home</a></p>
       <h1 className="main-title">{props.title}</h1>
     </nav>
   );
