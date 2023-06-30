e0f526c18c7a60b4c83f2379af73b65e3dcd072b
Camilo Munera <camilo.munerac@gmail.com>
2023-06-30 16:29:56 -0500

    COrreccion UPS finalizado - add Home button

--- Front/src/components/Navbar/Navbar.jsx
+++ Front/src/components/Navbar/Navbar.jsx
@@ -16,11 +16,11 @@ export function Navbar(props) {
   return (
     <nav className="navbar">
       <div className="logo">
-        <a href={`http://${envi}:4000/concentradora/dcs/home`}>
+        <a href="http://10.224.116.78:4000/concentradora/dcs/home">
           <img src="/candelaria.png" alt="Logo" />
         </a>
       </div>
-        <p className="home-link" ><a href={`http://${envi}:4000/concentradora/dcs/home`} style={{color:"white"}}>Home</a></p>
+        <p className="home-link" ><a href="http://10.224.116.78:4000/concentradora/dcs/home" style={{color:"white"}}>Home</a></p>
       <h1 className="main-title">{props.title}</h1>
     </nav>
   );
