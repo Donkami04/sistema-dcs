80a59295feb709a664d83ef5793f8c7375179d3e
Camilo Munera <camilo.munerac@gmail.com>
2023-07-12 12:12:37 -0500

    deploy mesh

--- Front/src/components/Navbar/Navbar.jsx
+++ Front/src/components/Navbar/Navbar.jsx
@@ -2,29 +2,15 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import "./navbar.css";
 
-const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT;
+// const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT;
 
-let envi;
-if (ENVIRONMENT === "local") {
-  envi = "localhost";
-} else if (ENVIRONMENT === "development") {
-  envi = "10.224.116.78";
-} else if (ENVIRONMENT === "production") {
-  envi = "10.224.116.14";
-}
-
-// export function Navbar(props) {
-//   return (
-//     <nav className="navbar">
-//       <div className="logo">
-//         <a href="http://10.224.116.78:4000/concentradora/dcs/home">
-//           <img src="/candelaria.png" alt="Logo" />
-//         </a>
-//       </div>
-//         <p className="home-link" ><a href="http://10.224.116.78:4000/concentradora/dcs/home" style={{color:"white"}}>Home</a></p>
-//       <h1 className="main-title">{props.title}</h1>
-//     </nav>
-//   );
+// let envi;
+// if (ENVIRONMENT === "local") {
+//   envi = "localhost";
+// } else if (ENVIRONMENT === "development") {
+//   envi = "10.224.116.78";
+// } else if (ENVIRONMENT === "production") {
+//   envi = "10.224.116.14";
 // }
 
 export function Navbar(props) {
