24cebc30fc317b86496877e259aac4aa4139df69
Camilo Munera <camilo.munerac@gmail.com>
2023-07-18 09:34:38 -0500

    Arreglado - cambio de pagina automatico

--- DCS-Candelaria/Services/Devices/main.py
+++ DCS-Candelaria/Services/Devices/main.py
@@ -1,4 +1,4 @@
-import warnings, requests, os, time, traceback, datetime, sched, re
+import warnings, requests, os, time, traceback, datetime, sched, re,logging
 import mysql.connector
 from dotenv import load_dotenv
 from config import database
@@ -6,7 +6,13 @@ from config import database
 # Esto evita que las respuestas de las API tengan warnings.
 warnings.filterwarnings('ignore', message='Unverified HTTPS request')
 
-def clients():
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler('issues.log')
+file_handler.setLevel(logging.WARNING)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logging.getLogger().addHandler(file_handler)
+
+def get_devices_data():
     load_dotenv()
     # env = os.getenv('ENVIRONMENT')
     
@@ -58,126 +64,128 @@ def clients():
         },
     ]
 
-    try:
-        load_dotenv()
-        
+    try:   
         for device in devices:
-            ip_device = device['ip']
+            ip_device = device['ip_device']
             device_type = device['type']
             site = device['site']
             ubication = device['dpto']
             
             URL_GET_ID = os.getenv('URL_PRTG_IP').format(ip=ip_device)
             response_get_id = requests.get(URL_GET_ID, verify=False).json()
-            # Inicia bloque para consultar datos a la API PRTG
-            # Si no hay ID se definen las variables como Not Found y no hace peticion GET
-            if len(response_get_id['devices'][0]['objid']) == 0:
+            if len(response_get_id['devices'][0]) == 0:
                 prtg_device = 'Not Found'
                 prtg_id_device = 'Not Found'
                 prtg_name_sensor = 'Not Found'
                 prtg_status = 'Not Found'
                 prtg_lastup = 'Not Found'
                 prtg_lastdown = 'Not Found'
+                logging.info(prtg_device)
+                logging.info(prtg_id_device)
+                logging.info(prtg_name_sensor)
+                logging.info(prtg_status)
+                logging.info(prtg_lastup)
+                logging.info(prtg_lastdown)
             else:
                 prtg_id_device = response_get_id['devices'][0]['objid']
-                URL_PRTG_ID = os.getenv('URL_PRTG_ID').format(id_device=prtg_id_device)
-                # prtg_sensor = URL_PRTG_ID
-                prtg_data = requests.get(URL_PRTG_ID, verify=False).json()
-                sensor = prtg_data['sensors'][0]
-                status_prtg = sensor['status']
-                last_up_prtg = sensor['lastup']
-                last_down_prtg = sensor['lastdown']
+                URL_PRTG_GET_DATA = os.getenv('URL_PRTG_ID').format(id_device=prtg_id_device)
+                response_prtg_data = requests.get(URL_PRTG_GET_DATA, verify=False).json()
+                sensor = response_prtg_data['sensors'][0]
+                prtg_name_sensor = sensor['name']
+                prtg_device = sensor['device']
+                prtg_status = sensor['status']
+                prtg_lastup = sensor['lastup']
+                prtg_lastdown = sensor['lastdown']
+                
                 patron = re.compile(r'<.*?>') # Se usa para formatear el last_up y last_down
-                last_up_prtg =  re.sub(patron, '', last_up_prtg)
-                last_down_prtg =  re.sub(patron, '', last_down_prtg)
-                
-            # Inicia bloque para consultar datos a la API CISCO PRIME
-            # Si no hay ID se definen las variables como Not Found y no hace peticion GET
-            if cisco_id_client == 'Not Found':
-                cisco_client_port = 'Not Found'
-                cisco_client_status = 'Not Found'
-                cisco_device_name = 'Not Found'
-                cisco_device_ip_adrress = 'Not Found'
-                cisco_device_reachability = 'Not Found'
-                prtg_device_status = 'Not Found'
-            else:
-                URL_CISCO_ID = os.getenv('URL_CISCO_ID').format(cisco_id_client=cisco_id_client)
-                cisco_client_response = requests.get(URL_CISCO_ID, verify=False).json()
-                cisco_client_data = cisco_client_response['queryResponse']['entity'][0]['clientsDTO']
-                cisco_client_port = cisco_client_data['clientInterface']
-                cisco_client_status = cisco_client_data['status']
-                
-                cisco_device_name = cisco_client_data['deviceName']
-                cisco_device_ip_adrress = cisco_client_data['deviceIpAddress']['address']
-                
-                # Con el 'cisco_device_ip_adrress' se busca encontrar el Status PRTG del SW. 
-                # Obtener el ID del PRTG mediante esta IP
-                # Obtener el status del PRTG mediante el ID obtenido anteriormente
-                prtg_device_ip_url = os.getenv('URL_PRTG_IP').format(ip=cisco_device_ip_adrress)
-                prtg_device_ip_response = requests.get(prtg_device_ip_url, verify=False).json()
-                
-                if prtg_device_ip_response['treesize'] == 0:
-                    prtg_device_status = 'Not Found'
-                
-                else:
-                    prtg_device_id = prtg_device_ip_response['devices'][0]['objid']
-                    prtg_device_id_url = os.getenv('URL_PRTG_ID').format(id_device=prtg_device_id)
-                    prtg_device_status_response = requests.get(prtg_device_id_url, verify=False).json()
-                    prtg_device_status = prtg_device_status_response['sensors'][0]['status']
-                
-                # Con el 'cisco_device_ip_adrress' de la API de cisco se obtiene el id del CISCO
-                # Y posteriormente se obtiene el 'REACHABILITY' del CISCO
-                cisco_device_ip_url = os.getenv('URL_CISCO_IP_DEVICE').format(ip=cisco_device_ip_adrress)
-                cisco_device_ip_response = requests.get(cisco_device_ip_url, verify=False).json()
-                if cisco_device_ip_response['queryResponse']['@count'] == 0:
-                    cisco_device_reachability = 'Not Found'
-                else:
-                    cisco_device_id = cisco_device_ip_response['queryResponse']['entityId'][0]['$']
-                    cisco_device_id_url = os.getenv('URL_CISCO_ID_DEVICE').format(id_device=cisco_device_id)
-                    cisco_device_id_response = requests.get(cisco_device_id_url, verify=False).json()
-                    cisco_device_reachability = cisco_device_id_response['queryResponse']['entity'][0]['devicesDTO']['reachability']
+                prtg_lastup =  re.sub(patron, '', prtg_lastup)
+                prtg_lastdown =  re.sub(patron, '', prtg_lastdown)
+                logging.info(prtg_name_sensor)
+                logging.info(prtg_device)
+                logging.info(prtg_status)
+                logging.info(prtg_lastup)
+                logging.info(prtg_lastdown)
 
-            # Inicia bloque para almacenar la informacion procesada en la base de datos
-            # Dependiendo del tipo de grupo se almacena en la tabla CSP, CSS, CNP, CNS, HSE, CNPB o CNSB
-            group_low = group.lower()
-            query = (f"INSERT INTO dcs.{group_low} (name, `group`, description, ip, status_prtg, lastup_prtg, lastdown_prtg, device_ip_cisco, device_cisco, port_cisco, status_cisco, reachability_cisco, id_prtg, importancia, clave, status_device_cisco)"
-                f"VALUES ('{name}', '{group}', '{description}', '{ip}', '{status_prtg}', '{last_up_prtg}', '{last_down_prtg}', '{cisco_device_ip_adrress}', '{cisco_device_name}', '{cisco_client_port}', '{cisco_client_status}', '{cisco_device_reachability}', '{id_device_prtg}', '{importancia}', '{clave}', '{prtg_device_status}')")
-            cursor.execute(query)
-            mydb.commit()
+            # if cisco_id_client == 'Not Found':
+            #     cisco_client_port = 'Not Found'
+            #     cisco_client_status = 'Not Found'
+            #     cisco_device_name = 'Not Found'
+            #     cisco_device_ip_adrress = 'Not Found'
+            #     cisco_device_reachability = 'Not Found'
+            #     prtg_device_status = 'Not Found'
+            # else:
+            #     URL_CISCO_ID = os.getenv('URL_CISCO_ID').format(cisco_id_client=cisco_id_client)
+            #     cisco_client_response = requests.get(URL_CISCO_ID, verify=False).json()
+            #     cisco_client_data = cisco_client_response['queryResponse']['entity'][0]['clientsDTO']
+            #     cisco_client_port = cisco_client_data['clientInterface']
+            #     cisco_client_status = cisco_client_data['status']
+                
+            #     cisco_device_name = cisco_client_data['deviceName']
+            #     cisco_device_ip_adrress = cisco_client_data['deviceIpAddress']['address']
+
+            #     prtg_device_ip_url = os.getenv('URL_PRTG_IP').format(ip=cisco_device_ip_adrress)
+            #     prtg_device_ip_response = requests.get(prtg_device_ip_url, verify=False).json()
+                
+            #     if prtg_device_ip_response['treesize'] == 0:
+            #         prtg_device_status = 'Not Found'
+                
+            #     else:
+            #         prtg_device_id = prtg_device_ip_response['devices'][0]['objid']
+            #         prtg_device_id_url = os.getenv('URL_PRTG_ID').format(id_device=prtg_device_id)
+            #         prtg_device_status_response = requests.get(prtg_device_id_url, verify=False).json()
+            #         prtg_device_status = prtg_device_status_response['sensors'][0]['status']
+                
+            #     cisco_device_ip_url = os.getenv('URL_CISCO_IP_DEVICE').format(ip=cisco_device_ip_adrress)
+            #     cisco_device_ip_response = requests.get(cisco_device_ip_url, verify=False).json()
+            #     if cisco_device_ip_response['queryResponse']['@count'] == 0:
+            #         cisco_device_reachability = 'Not Found'
+            #     else:
+            #         cisco_device_id = cisco_device_ip_response['queryResponse']['entityId'][0]['$']
+            #         cisco_device_id_url = os.getenv('URL_CISCO_ID_DEVICE').format(id_device=cisco_device_id)
+            #         cisco_device_id_response = requests.get(cisco_device_id_url, verify=False).json()
+            #         cisco_device_reachability = cisco_device_id_response['queryResponse']['entity'][0]['devicesDTO']['reachability']
+
+            # group_low = group.lower()
+            # query = (f"INSERT INTO dcs.{group_low} (name, `group`, description, ip, prtg_status, lastup_prtg, lastdown_prtg, device_ip_cisco, device_cisco, port_cisco, status_cisco, reachability_cisco, id_prtg, importancia, clave, status_device_cisco)"
+            #     f"VALUES ('{name}', '{group}', '{description}', '{ip}', '{prtg_status}', '{prtg_lastup}', '{prtg_lastdown}', '{cisco_device_ip_adrress}', '{cisco_device_name}', '{cisco_client_port}', '{cisco_client_status}', '{cisco_device_reachability}', '{id_device_prtg}', '{importancia}', '{clave}', '{prtg_device_status}')")
+            # cursor.execute(query)
+            # mydb.commit()
                 
         # Bloque para guardar el datetime de la ultima consulta en la tabla 'fechas_consultas_clientes'.         
-        now = datetime.datetime.now()
-        fecha_y_hora = now.strftime("%Y-%m-%d %H:%M:%S")
-        fecha_y_hora = str(fecha_y_hora)
-        cursor.execute(f"INSERT INTO dcs.fechas_consultas_clientes (ultima_consulta, estado) VALUES ('{fecha_y_hora}', 'OK')")
-        mydb.commit()
+        # now = datetime.datetime.now()
+        # fecha_y_hora = now.strftime("%Y-%m-%d %H:%M:%S")
+        # fecha_y_hora = str(fecha_y_hora)
+        # cursor.execute(f"INSERT INTO dcs.fechas_consultas_clientes (ultima_consulta, estado) VALUES ('{fecha_y_hora}', 'OK')")
+        # mydb.commit()
         
-        with open("/app/logs.txt", "a") as archivo:
-            archivo.write(str(fecha_y_hora) + '\n')
+        # with open("/app/logs.txt", "a") as archivo:
+        #     archivo.write(str(fecha_y_hora) + '\n')
                 
         print('Terminado')       
-        cursor.close()
+        # cursor.close()
                 
     except Exception:
         print(traceback.format_exc())
-        now = datetime.datetime.now()
-        fecha_y_hora = now.strftime("%Y-%m-%d %H:%M:%S")
-        fecha_y_hora = str(fecha_y_hora)
-        cursor.execute(f"INSERT INTO dcs.fechas_consultas_clientes (ultima_consulta, estado) VALUES ('{fecha_y_hora}', 'ERROR')")
-        mydb.commit()
-        cursor.close()
+        # now = datetime.datetime.now()
+        # fecha_y_hora = now.strftime("%Y-%m-%d %H:%M:%S")
+        # fecha_y_hora = str(fecha_y_hora)
+        # cursor.execute(f"INSERT INTO dcs.fechas_consultas_clientes (ultima_consulta, estado) VALUES ('{fecha_y_hora}', 'ERROR')")
+        # mydb.commit()
+        # cursor.close()
         
-        with open("/app/logs.txt", "a") as archivo:
-            archivo.write('Fecha y hora del error: ' + str(fecha_y_hora) + ' Dispositivo del error ---> ' + ip + '\n')
-            archivo.write(traceback.format_exc())
-            archivo.write("\n")
+        # with open("/app/logs.txt", "a") as archivo:
+        #     archivo.write('Fecha y hora del error: ' + str(fecha_y_hora) + ' Dispositivo del error ---> ' + ip_device + '\n')
+        #     archivo.write(traceback.format_exc())
+        #     archivo.write("\n")
                 
                 
-def bucle(scheduler):
-    clients()
-    scheduler.enter(300, 1, bucle, (scheduler,))
+# def bucle(scheduler):
+#     clients()
+#     scheduler.enter(300, 1, bucle, (scheduler,))
 
-if __name__ == '__main__':
-    s = sched.scheduler(time.time, time.sleep)
-    s.enter(0, 1, bucle, (s,))
-    s.run()
+# if __name__ == '__main__':
+#     s = sched.scheduler(time.time, time.sleep)
+#     s.enter(0, 1, bucle, (s,))
+#     s.run()
+
+get_devices_data()
--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,26 +1,39 @@
-import { Routes, Route } from "react-router-dom";
-import {Home} from "./components/Home/Home";
-import {Dcs} from "./components/Dcs/Dcs";
-import {Switches} from "./components/Dcs-switches/DcsSwitches";
-import {Ups} from "./components/Ups/Ups";
-import { Vpn } from "./components/Vpn/Vpn"
-import { Mesh } from "./components/Mesh/Mesh"
-import "./app.css";
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import { Dcs } from './components/Dcs/Dcs';
+import { Switches } from './components/Dcs-switches/DcsSwitches';
+import { Ups } from './components/Ups/Ups';
+import { Vpn } from './components/Vpn/Vpn';
+import { Mesh } from './components/Mesh/Mesh';
+import './app.css';
 
 function App() {
   const titleDCS = 'Concentradora';
   const titleVpn = 'Monitoreo VPN';
   const titleHome = 'Sistema de monitoreo';
 
+  const location = useLocation();
+
+  useEffect(() => {
+    const refreshInterval = setInterval(() => {
+      window.location.reload();
+    }, 5 * 60 * 1000); // 5 minutos en milisegundos
+
+    return () => {
+      clearInterval(refreshInterval); // Limpiar el intervalo al desmontar el componente
+    };
+  }, []);
+
   return (
     <div className="MainContainer">
-      <Routes >
-        <Route path="monitoreo/home" element={<Home />} />
-        <Route path="monitoreo/candelaria/clients" element={<Dcs title={titleDCS}/>} />
-        <Route path="monitoreo/candelaria/switches" element={<Switches title={titleDCS}/>} />
-        <Route path="monitoreo/ups" element={<Ups />} />
-        <Route path="monitoreo/candelaria/mesh" element={<Mesh />} />
-        <Route path="monitoreo/vpn" element={<Vpn title={titleVpn}/>} />
+      <Routes location={location}>
+        <Route path="/monitoreo/home" element={<Home />} />
+        <Route path="/monitoreo/candelaria/clients" element={<Dcs title={titleDCS} />} />
+        <Route path="/monitoreo/candelaria/switches" element={<Switches title={titleDCS} />} />
+        <Route path="/monitoreo/ups" element={<Ups />} />
+        <Route path="/monitoreo/candelaria/mesh" element={<Mesh />} />
+        <Route path="/monitoreo/vpn" element={<Vpn title={titleVpn} />} />
       </Routes>
     </div>
   );
