1734fe9c90f26d925e43563eb041db92aebef263
Camilo Munera <camilo.munerac@gmail.com>
2023-08-30 13:19:12 -0500

    Home, mesh, ups, devices

--- Backend/controllers/indicators/devices.js
+++ Backend/controllers/indicators/devices.js
@@ -1,72 +1,78 @@
 const { getDevices } = require("../devices");
 
 const dashboardDevices = async () => {
-    try {
-  let devices = await getDevices();
-  devices = devices.map((device) => device.toJSON());
+  try {
+    let devices = await getDevices();
+    devices = devices.map((device) => device.toJSON());
 
-  const numCamerasUp = devices.filter(
-    (device) =>
-      device.type.toLowerCase() === "camara" &&
-      device.prtg_status.includes("Up")
-  );
-  const numCamerasDown = devices.filter(
-    (device) =>
-      device.type.toLowerCase() === "camara" &&
-      device.prtg_status.includes("Down")
-  );
-  const numTotalCameras = devices.filter(
-    (device) =>
-      device.type.toLowerCase() === "camara"
-  );
-  const numApUp = devices.filter(
-    (device) =>
-      device.type.toLowerCase() === "ap" && device.prtg_status.includes("Up")
-  );
-  const numApDown = devices.filter(
-    (device) =>
-      device.type.toLowerCase() === "ap" && device.prtg_status.includes("Down")
-  );
-  const numTotalAp = devices.filter(
-    (device) =>
-      device.type.toLowerCase() === "ap"
-  );
-  const numOthersUp = devices.filter(
-    (device) =>
-      device.type.toLowerCase() !== "camara" &&
-      device.type.toLowerCase() !== "ap" &&
-      device.prtg_status.includes("Up")
-  );
-  const numOthersDown = devices.filter(
-    (device) =>
-      device.type.toLowerCase() !== "camara" &&
-      device.type.toLowerCase() !== "ap" &&
-      device.prtg_status.includes("Down")
-  );
-  const numTotalOthers = devices.filter(
-    (device) =>
-      device.type.toLowerCase() !== "ap" &&
-      device.type.toLowerCase() !== "camara"
-  );
+    const numCamerasUp = devices.filter(
+      (device) =>
+        device.type.toLowerCase() === "camara" &&
+        device.prtg_status.includes("Up")
+    );
+    const numCamerasDown = devices.filter(
+      (device) =>
+        device.type.toLowerCase() === "camara" &&
+        device.prtg_status.includes("Down")
+    );
+    // const numTotalCameras = devices.filter(
+    //   (device) => device.type.toLowerCase() === "camara"
+    // );
 
-  const dataDevices = {
-    numTotalDevices: devices.length,
-    numTotalCameras: numTotalCameras.length,
-    numTotalAp: numTotalAp.length,
-    numTotalOthers: numTotalOthers.length,
-    numCamerasUp: numCamerasUp.length,
-    numCamerasDown: numCamerasDown.length,
-    numApUp: numApUp.length,
-    numApDown: numApDown.length,
-    numOthersUp: numOthersUp.length,
-    numOthersDown: numOthersDown.length,
-  };
+    const numTotalCameras = numCamerasUp.length + numCamerasDown.length;
+    
+    const numApUp = devices.filter(
+      (device) =>
+        device.type.toLowerCase() === "ap" && device.prtg_status.includes("Up")
+    );
+    const numApDown = devices.filter(
+      (device) =>
+        device.type.toLowerCase() === "ap" &&
+        device.prtg_status.includes("Down")
+    );
+    // const numTotalAp = devices.filter(
+    //   (device) => device.type.toLowerCase() === "ap"
+    // );
+    const numTotalAp = numApUp.length + numApDown.length;
+    
+    const numOthersUp = devices.filter(
+      (device) =>
+        device.type.toLowerCase() !== "camara" &&
+        device.type.toLowerCase() !== "ap" &&
+        device.prtg_status.includes("Up")
+    );
+    const numOthersDown = devices.filter(
+      (device) =>
+        device.type.toLowerCase() !== "camara" &&
+        device.type.toLowerCase() !== "ap" &&
+        device.prtg_status.includes("Down")
+    );
+    // const numTotalOthers = devices.filter(
+    //   (device) =>
+    //     device.type.toLowerCase() !== "ap" &&
+    //     device.type.toLowerCase() !== "camara" &&
+    //     !device.prtg_status.includes("Paused") &&
+    //     device.prtg_status !== "Not Found"
+    // );
+    const numTotalOthers = numOthersUp.length + numOthersDown.length;
 
-  return dataDevices;
+    const dataDevices = {
+      numTotalDevices: devices.length,
+      numTotalCameras: numTotalCameras,
+      numTotalAp: numTotalAp,
+      numTotalOthers: numTotalOthers,
+      numCamerasUp: numCamerasUp.length,
+      numCamerasDown: numCamerasDown.length,
+      numApUp: numApUp.length,
+      numApDown: numApDown.length,
+      numOthersUp: numOthersUp.length,
+      numOthersDown: numOthersDown.length,
+    };
 
-  } catch (error){
+    return dataDevices;
+  } catch (error) {
     console.error(error);
-    return error
+    return error;
   }
 };
 
